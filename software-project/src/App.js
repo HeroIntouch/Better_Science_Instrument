5909a49fadd886e6afaac875869f346d7a3e5c0a
nattamon chinprasatsak <furuku@nattamons-MacBook-Pro.local>
2019-10-11 04:23:34 +0700

    create promotion

--- software-project/src/App.js
+++ software-project/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import NavBar from './Tool/navBar';
+import Button from './Tool/button';
 import './App.css';
 
 function App() {
   return (
+    
+    <div >
     <NavBar/>
+    <div style = {{padding: 10}}>
+    <div >
+    <Button/>
+    </div>
+    
+    </div>
+    
+    
+    </div>
+    
+    
   );
 }
 
--- software-project/src/Tool/button.js
+++ software-project/src/Tool/button.js
@@ -5,13 +5,9 @@ export default class Example extends React.Component {
   render() {
     return (
       <div>
-        <Button color="primary">primary</Button>{' '}
-        <Button color="secondary">secondary</Button>{' '}
-        <Button color="success">success</Button>{' '}
-        <Button color="info">info</Button>{' '}
-        <Button color="warning">warning</Button>{' '}
-        <Button color="danger">danger</Button>{' '}
-        <Button color="link">link</Button>
+        <h1>      Promotion </h1>
+        <Button outline color="secondary" block style = {{height: 400}}>edit</Button>
+        
       </div>
     );
   }
