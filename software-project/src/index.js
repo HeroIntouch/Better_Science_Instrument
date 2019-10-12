3c400264343830aff746a02d38f171ee8f648dc2
khachen <khachen.t@ku.th>
2019-10-12 22:24:46 +0700

    :dog:

--- software-project/src/index.js
+++ software-project/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Theme from './theme';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Router,Route,Link,browserHistory} from 'react-router';
+import {Router,Route,browserHistory} from 'react-router';
 
 ReactDOM.render(
     <Router history={browserHistory}>
         {/* <Route path="/homepage" component={homepage}/>
         <Route path="/product" component={Product}/>
         <Route path="/user" component={User}/> */}
-        <Route path="/theme" component={Theme}/>
+        <Route path="/" component={Theme}/>
     </Router>,document.getElememtByID('root')
     );
