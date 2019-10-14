e9c986ef202ed8eb8d354b26f0b8f169b4ef5dda
khachen <khachen.t@ku.th>
2019-10-14 17:39:04 +0700

    index.js modify

--- software-project/src/index.js
+++ software-project/src/index.js
@@ -2,15 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Theme from './theme';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import {Router,Route,browserHistory} from 'react-router';
-
-// ReactDOM.render(
-//     <Router history={browserHistory}>
-//         <Route path="/homepage" component={homepage}/>
-//         <Route path="/product" component={Product}/>
-//         <Route path="/user" component={User}/>
-//         <Route path="/" component={Theme}/>
-//     </Router>,document.getElememtByID('root')
-//     );
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Theme />, document.getElementById('root'));
+serviceWorker.unregister();
