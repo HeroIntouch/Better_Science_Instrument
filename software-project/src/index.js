5b17ab43148b2a15b6cbca52ca74e53fc19c0abd
khachen <khachen.t@ku.th>
2019-11-02 20:55:02 +0700

    fix index.js

--- software-project/src/index.js
+++ software-project/src/index.js
@@ -8,12 +8,10 @@ import { createStore } from 'redux';
 import allReducer from './reducer';
 import { Provider } from 'react-redux';
 
-
 const store = createStore(
     allReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
- 
 
 ReactDOM.render(
     <Provider store={store}>
