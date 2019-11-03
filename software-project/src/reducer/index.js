5f32cad14d3f119a81d9f1fe570019244cb98bf7
khachen <khachen.t@ku.th>
2019-11-03 14:32:50 +0700

    add color

--- software-project/src/reducer/index.js
+++ software-project/src/reducer/index.js
@@ -1,10 +1,12 @@
 import changeBGColorReducer from './changeColorReducer';
 import changeTXColorReducer from './changeTXColorReducer';
+import changeCodeColorReducer from './changeCodeColorReducer'
 import { combineReducers } from 'redux';
 
 const allReducer = combineReducers({
     BGcolor : changeBGColorReducer,
-    TXcolor : changeTXColorReducer
+    TXcolor : changeTXColorReducer,
+    codeBGcolor : changeCodeColorReducer
 });
 
 export default allReducer
