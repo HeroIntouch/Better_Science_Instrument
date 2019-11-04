d86e367a1271b6dc8a4a31709b594078514615e4
Unpolygon <saengsetth@outlook.com>
2019-11-04 15:11:36 +0700

    edit Homepage

--- software-project/src/Homepage/App.js
+++ software-project/src/Homepage/App.js
@@ -4,19 +4,45 @@ import NavBar from './navbar/navBar'
 import Promotion from './promotion/promotion'
 import Media from './media/media'
 import Generate from '../Generate/Generate'
+import { db } from '../firebase';
 
-function App() {
-  return (
-    <div>
-      <div><NavBar /></div>
-      <div className = 'promo'><Promotion /></div>
-      {/* <div className = 'tabs'><Tabs /></div> */}
-      <div className = 'tabs'><Media /></div>
-      <div className = 'contact'>Mission</div>
-      <div className = 'contact'>Contact Us</div>
-      <Generate></Generate>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Mission: 'Hello',
+      Contact: 'Hello'
+    }
+  }
+
+  
+
+  render() {
+    db.collection('Promotion').get().then(snapshot => {
+      snapshot.docs.forEach(doc => {
+        if(doc.id == 1){
+          this.setState({Mission: doc.data().Link})
+        }
+        if(doc.id == 2){
+          this.setState({Contact: doc.data().Link})
+        }
+      })
+    })
+
+
+    return (
+      <div>
+        <div><NavBar /></div>
+        <div className='promo'><Promotion s1 = {this.state.Mission}/></div>
+        {/* <div className = 'tabs'><Tabs /></div> */}
+        <div className='tabs'><Media /></div>
+        <div className='contact' >{this.state.Mission}</div>
+        <div className='contact'>{this.state.Contact}</div>
+        <Generate></Generate>
+      </div>
+    );
+  }
 }
 
 export default App;
